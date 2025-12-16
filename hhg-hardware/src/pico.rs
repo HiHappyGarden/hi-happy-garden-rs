59219e6676320d965ffa85eedf124e4351b5a1da
Antonio Salsi <passy.linux@zresa.it>
2025-12-16 22:24:26 +0100

    feat: replace TaskHandle_t with ThreadHandle in stack overflow hook

--- hhg-hardware/src/pico.rs
+++ hhg-hardware/src/pico.rs
@@ -1,11 +1,7 @@
 mod hardware;
 
-
 use core::ffi::c_char;
-
-// TaskHandle_t is a pointer to void in FreeRTOS
-#[allow(non_camel_case_types)]
-type TaskHandle_t = *mut core::ffi::c_void;
+use osal_rs::os::types::ThreadHandle;
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn vApplicationMallocFailedHook() -> ! {
@@ -18,9 +14,7 @@ pub unsafe extern "C" fn vApplicationIdleHook() -> ! {
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn vApplicationStackOverflowHook(_x_task: TaskHandle_t, _pc_task_name: *mut c_char) -> !{
-    // Equivalent to: (void) xTask; (void) pcTaskName;
-    // The underscore prefix already marks these as intentionally unused in Rust
+pub unsafe extern "C" fn vApplicationStackOverflowHook(_x_task: ThreadHandle, _pc_task_name: *mut c_char) -> !{
     loop {}
 }
 
