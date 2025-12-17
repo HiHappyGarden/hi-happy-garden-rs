4c22bfd15c2f91ab3ef2372e899f90d6d8fcf1f8
Antonio Salsi <passy.linux@zresa.it>
2025-12-18 00:04:34 +0100

    feat: clean up hardware.rs by removing unused imports and example-related code; update osal-rs subproject to latest commit

--- hhg-hardware/src/pico/hardware.rs
+++ hhg-hardware/src/pico/hardware.rs
@@ -1,29 +1,9 @@
 
-use osal_rs::os::{System, SystemFn, Thread, ThreadFn};
-
-unsafe extern "C" {
-    fn printf(format: *const u8, ...) -> i32;
-}
-
-#[cfg(feature = "examples")]
-extern "C" {
-    fn xSemaphoreCreateRecursiveMutex() -> *mut core::ffi::c_void;
-    fn xQueueTakeMutexRecursive(mutex: *mut core::ffi::c_void, ticks: u32) -> i32;
-    fn xQueueGiveMutexRecursive(mutex: *mut core::ffi::c_void) -> i32;
-}
-
-#[cfg(feature = "examples")]
-const PD_TRUE: i32 = 1;
-#[cfg(feature = "examples")]
-const PORT_MAX_DELAY: u32 = 0xFFFFFFFF;
-
-#[cfg(feature = "examples")]
-static mut EXAMPLES_THREAD: Option<Thread> = None;
+use osal_rs::os::{System, SystemFn};
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn hardware_main() {
 
-    printf(b"Starting RTOS...\n\0".as_ptr());
 }
 
 
