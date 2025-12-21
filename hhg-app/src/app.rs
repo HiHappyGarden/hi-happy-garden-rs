d7cb3d86fe9d0290e3699bb6fea755b19bef4a49
Antonio Salsi <passy.linux@zresa.it>
2025-12-21 15:36:11 +0100

    feat: add support for tests in CMake and Cargo; implement logging for thread creation in app and hardware modules; update osal-rs subproject to latest commit

--- hhg-app/src/app.rs
+++ hhg-app/src/app.rs
@@ -1,5 +1,33 @@
+use alloc::boxed::Box;
+use alloc::sync::Arc;
+
+use osal_rs::os::{Thread, ThreadFn, ThreadParam};
+
+use osal_rs::utils::Result;
+use osal_rs::{log_info};
+
+
+
+static mut APP_THREAD: Option<Thread> = None;
+
+const APP_TAG: &str = "app_main";
+
+
+fn app_main_thread(_thread: Box<dyn ThreadFn>, _param: Option<ThreadParam>) -> Result<ThreadParam>{
+    Ok(Arc::new(()))
+}
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn app_main() {
+
+    log_info!(APP_TAG, "Creating pico hardware test thread...");
+
+    match Thread::new("app_main_thread", 4096, 3, app_main_thread).spawn(None) {
+        Ok(spawned) =>  {
+            log_info!(APP_TAG, "Thread spawned successfully!");
+            unsafe  { APP_THREAD = Some(spawned)}
+        }
+        Err(e) => panic!("Failed to spawn app_main_thread: {:?}", e)
+    };
 
 }
--- hhg-hardware/src/pico/hardware.rs
+++ hhg-hardware/src/pico/hardware.rs
@@ -1,43 +1,80 @@
-
-use osal_rs::os::{System, SystemFn, Thread, ThreadFn};
-use osal_rs::log::set_enable_color;
 use alloc::boxed::Box;
+#[allow(unused_imports)]
+use core::any::Any;
+
+#[allow(unused_imports)]
+use alloc::sync::Arc;
+use osal_rs::os::{System, SystemFn, Thread, ThreadFn, ThreadParam};
+use osal_rs::log::set_enable_color;
+#[allow(unused_imports)]
+use osal_rs::utils::Result;
+use osal_rs::{log_info};
+
+ #[cfg(not(feature = "tests"))]
+static HARDWARE_THREAD: Option<Thread> = None;
+
+const APP_TAG: &str = "hardware_main";
+
+
+ #[cfg(not(feature = "tests"))]
+fn hardware_main_thread(_thread: Box<dyn ThreadFn>, param: Option<ThreadParam>) -> Result<ThreadParam>{
+    Ok(Arc::new(()))
+}
+
+
 
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn hardware_main() {
     set_enable_color(false);
 
+    #[cfg(not(feature = "tests"))]
+    {
+        log_info!(APP_TAG, "Creating pico hardware test thread...");
+
+        match Thread::new("hardware_main_thread", 4096, 3, hardware_main_thread).spawn(None) {
+            Ok(_spawned) =>  log_info!(APP_TAG, "Thread spawned successfully!"),
+            Err(e) => panic!("Failed to spawn hardware_main_thread: {:?}", e)
+        };
+    }
 
 
 
     #[cfg(feature = "tests")]
     {
-        const APP_TAG: &str = "hardware_main";
-        use osal_rs::{log_info};
-
-        log_info!(APP_TAG, "Creating test_runner thread...");
-
-        match Thread::new("test_runner", 4096, 3, Box::new(|_, _| {
-            use osal_rs::utils::Error;
-
-
-            match osal_rs_tests::freertos::run_all_tests() {
-                Ok(_) => log_info!(APP_TAG, "All tests passed!"),
-                Err(e) => panic!("Tests failed with error: {:?}", e)
-            };
-
-            Err(Error::Unhandled(""))
-        })).spawn(None) {
-            Ok(_spawned) =>  log_info!(APP_TAG, "Thread spawned successfully!"),
-            Err(e) => panic!("Failed to spawn test_runner thread: {:?}", e)
-        };
-        
+        perform_tests();
     }
 }
+
+
+
+
+
 
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn hardware_start_os() {
     System::start();
+}
+
+#[cfg(feature = "tests")]
+fn perform_tests() {
+
+
+    log_info!(APP_TAG, "Creating osal rs test thread...");
+
+    match Thread::new("osal_rs_test", 4096, 3, Box::new(|_, _| {
+        use osal_rs::utils::Error;
+
+
+        match osal_rs_tests::freertos::run_all_tests() {
+            Ok(_) => log_info!(APP_TAG, "All tests passed!"),
+            Err(e) => panic!("Tests failed with error: {:?}", e)
+        };
+
+        Err(Error::Unhandled(""))
+    })).spawn(None) {
+        Ok(_spawned) =>  log_info!(APP_TAG, "Thread spawned successfully!"),
+        Err(e) => panic!("Failed to spawn osal rs test thread: {:?}", e)
+    };
 }
