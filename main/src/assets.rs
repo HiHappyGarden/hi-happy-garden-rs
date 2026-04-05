cc07e85fc5b01c410fa3c1bc645eb9250e5f874b
Antonio Salsi <passy.linux@zresa.it>
2026-04-05 15:51:54 +0200

    fix: change module visibility to crate level for asset modules

--- main/src/assets.rs
+++ main/src/assets.rs
@@ -1,10 +1,10 @@
 
-pub mod font_5x8;
-pub mod font_8x8;
-pub mod ic_check_off;
-pub mod ic_check_on;
-pub mod ic_wifi_excellent; 
-pub mod ic_wifi_good;
-pub mod ic_wifi_fair;
-pub mod ic_wifi_no_signal;
-pub mod types;
+pub(crate) mod font_5x8;
+pub(crate) mod font_8x8;
+pub(crate) mod ic_check_off;
+pub(crate) mod ic_check_on;
+pub(crate) mod ic_wifi_excellent; 
+pub(crate) mod ic_wifi_good;
+pub(crate) mod ic_wifi_fair;
+pub(crate) mod ic_wifi_no_signal;
+pub(crate) mod types;
