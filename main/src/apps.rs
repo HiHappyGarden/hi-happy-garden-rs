583237d1c98ad78a40d0c334e9681f1dc489ac08
Antonio Salsi <passy.linux@zresa.it>
2026-04-14 23:18:28 +0200

    Update license comments across multiple files to remove the option clause for the GNU General Public License.
    - Modified license comments in various Rust and C source files to standardize the phrasing to "any later version" instead of "(at your option) any later version."
    - Added a new file `system_handler.rs` with the appropriate license header.

--- main/src/apps.rs
+++ main/src/apps.rs
@@ -6,7 +6,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -25,6 +25,7 @@ mod parser;
 mod screen_route;
 mod session;
 mod signals;
+mod system_handler;
 mod wifi;
 
 pub use display::DISPLAY_INPUT_MAX_SIZE;
